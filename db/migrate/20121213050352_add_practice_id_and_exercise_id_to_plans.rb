37e8642359dc0047e5efe7a9dbd41541b1c73a1d
Jake Siddall <jacobsiddall@gmail.com>
2012-12-21 22:29:56 -0600

    removed migration files related to Plans and Practice tables

--- db/migrate/20121213050352_add_practice_id_and_exercise_id_to_plans.rb
+++
@@ -1,6 +0,0 @@
-class AddPracticeIdAndExerciseIdToPlans < ActiveRecord::Migration
-  def change
-  	add_column :plans, :practice_id, :integer
-  	add_column :plans, :exercise_id, :integer
-  end
-end
--- db/migrate/20121221030212_drop_plans_and_practice_tables_and_remove_related_id_fields.rb
+++
@@ -1,7 +0,0 @@
-class DropPlansAndPracticeTablesAndRemoveRelatedIdFields < ActiveRecord::Migration
-  def change
-  	drop_table(:plans)
-  	drop_table(:practices)
-
-  end
-end
